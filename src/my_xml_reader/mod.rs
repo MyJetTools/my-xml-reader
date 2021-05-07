23360a01f5cc56b8b8b52161fe8e48eef870a43b
Andrey <amigin@gmail.com>
2021-05-07 11:38:34 +0300

    updated structure

---
+++ src/my_xml_reader/mod.rs
@@ -0,0 +1,11 @@
+mod my_xml_node;
+mod my_xml_reader;
+mod xml_tag_info;
+pub mod xml_utils;
+
+pub use my_xml_node::MyXmlNode;
+pub use xml_tag_info::XmlTagInfo;
+pub use xml_tag_info::XmlTagType;
+
+pub use my_xml_reader::MyXmlReader;
+pub use my_xml_reader::OpenedNode;
